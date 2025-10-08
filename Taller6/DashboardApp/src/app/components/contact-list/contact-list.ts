f1e2b9586d41b4184e9becc8790cd20fa71bd31f
Jairo <jairospm2002@gmail.com>
2025-10-07 19:16:02 -0500

    Finalizado taller 8

--- Taller6/DashboardApp/src/app/components/contact-list/contact-list.ts
+++ Taller6/DashboardApp/src/app/components/contact-list/contact-list.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import {ContactRow} from '../contact-row/contact-row';
+import { Recursos } from '../../servicios/recursos';
 
 
 
@@ -9,15 +10,27 @@ import {ContactRow} from '../contact-row/contact-row';
   templateUrl: './contact-list.html',
   styleUrl: './contact-list.css',
 })
-export class ContactList {
-  
+export class ContactList implements OnInit{
+  contacts: Contact[] = [];
+
+  constructor(private recursos:Recursos){}
+
+  ngOnInit(): void {
+    this.recursos.obtenerInfo().subscribe(
+      {
+        next:(datos)=>this.contacts=datos,
+        error:(err)=> console.error("Error al cargar los datos", err)
+      }
+    )
+  }
+  /*
   contacts: Contact[] = [
     {id: 1, name: 'Maria Lopez', email: 'maria@example.com'},
     {id: 2, name: 'Carlos Ruiz', email: 'carlos@example.com'},
     {id: 3, name: 'Ana Gomez', email: 'ana@example.com'},
     {id: 4, name: 'Luis Fernandez', email: 'luis@example.com'}
   ];
-  
+  */
 }
 
 
--- Taller6/DashboardApp/src/app/servicios/recursos.ts
+++ Taller6/DashboardApp/src/app/servicios/recursos.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Contact } from '../components/contact-list/contact-list';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Recursos {
-  private apiUrl
+  private apiUrl = "https://raw.githubusercontent.com/Rezep27/TalleresDesarrolloWeb/refs/heads/main/servicios/recursos.json"
+
+  constructor(private http: HttpClient) {}
+  obtenerInfo(): Observable<Contact[]> {
+  return this.http.get<Contact[]>(this.apiUrl);
+  }
 }
